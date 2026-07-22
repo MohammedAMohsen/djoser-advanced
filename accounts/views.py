fa593c883254a7162479800a282baac522442c7f
MohammedAMohsen <145121775+MohammedAMohsen@users.noreply.github.com>
2026-07-22 19:44:10 +0300

    Customize registration response format

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from djoser.views import UserViewSet
 from .serializers import CustomUserSerializer, CustomUserUpdateSerializer
+from .utils import success_response
+from rest_framework import status
 
 
 class CustomUserViewSet(UserViewSet):
@@ -10,3 +12,14 @@ class CustomUserViewSet(UserViewSet):
                 return CustomUserUpdateSerializer
             return CustomUserSerializer
         return super().get_serializer_class()
+    
+    def create(self, request, *args, **kwargs):
+        """
+        Override Djoser's register endpoint response.
+        """
+        response = super().create(request, *args, **kwargs)
+        return success_response(
+            data=response.data,
+            message="Account created successfully.",
+            status_code=status.HTTP_201_CREATED,
+        )
